06db6f640563fe806c37fc2986a54c9ff4e95958
Igor Shadurin <igor.shadurin@gmail.com>
2020-09-04 01:16:20 +0300

    Initial Commit

---
+++ Zoomerok/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Zoomerok
+//
+//  Created by sdancer on 9/4/20.
+//  Copyright © 2020 Shadurin Organization. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

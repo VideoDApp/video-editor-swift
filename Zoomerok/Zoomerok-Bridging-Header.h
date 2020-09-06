867cfd058996e4eeb7446b14f39a068323293ac8
Igor Shadurin <igor.shadurin@gmail.com>
2020-09-06 16:47:30 +0300

    added pods + GA

---
+++ Zoomerok/Zoomerok-Bridging-Header.h
@@ -0,0 +1,6 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <GoogleAnalytics/GAI.h>
+#import <GoogleAnalytics/GAIDictionaryBuilder.h>
